ab0fde24efd80c83fa8b75dd47a1acfeb9163a84
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-04-11 22:15:03 +0200

    Add getting started links

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,5 +1,18 @@
 <?php snippet('header') ?>
 
+<h4>Par où commencer ?</h4>
+<p>L’accessibilité commence dès la phase de conception.</p>
+
+<ul>
+	<?php foreach ($page->getting_started()->toStructure()->limit(3) as $ressource): ?>
+		<li><a href="<?= $ressource->url() ?>">
+			<b><?= $ressource->title() ?></b><br />
+			<?= $ressource->desc() ?>
+			</a>
+		</li>
+	<?php endforeach ?>
+</ul>
+
 <div class="posts">
 	<?php foreach (page('ressources')->ressources()->toStructure()->limit(4) as $ressource): ?>
 	  	<article>
