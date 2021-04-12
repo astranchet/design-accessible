b41602b0511fe6f8b3dfc76bc3af46692b8202b9
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-04-12 03:03:24 +0200

    Add accessibility page

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,6 +1,5 @@
 <ul>
-	<!-- TODO : link -->
-	<li><a href="">Accessibilité</a></li>
+	<li><a href="<?= page('accessibility')->url() ?>">Accessibilité</a></li>
 	<li><a href="<?= page('about')->url() ?>">À propos</a></li>
 	<li>Fait avec amour 💜</li>
 </ul>		
