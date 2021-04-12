55b42e0ad0596a1fd1bc96bbf9acea4a7a3e7354
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-04-12 03:07:14 +0200

    Add licence in footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,5 +1,9 @@
 <ul>
 	<li><a href="<?= page('accessibility')->url() ?>">Accessibilité</a></li>
 	<li><a href="<?= page('about')->url() ?>">À propos</a></li>
-	<li>Fait avec amour 💜</li>
+</ul>
+<ul>
+	<!-- TODO lien vers la license -->
+	<li>Attribution - Pas d’Utilisation Commerciale - Partage dans les Mêmes Conditions 4.0 International</li>
+	<li>Conçu avec amour 💜</li>
 </ul>		
