755bbad9f3a64b4834acfbba8c84f064c304d60a
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-04-13 20:53:42 +0200

    Order ressources

--- site/templates/ressources.php
+++ site/templates/ressources.php
@@ -5,7 +5,7 @@
 <p><b><?= $page->ressources()->toStructure()->count() ?> ressources</b> sélectionnées avec amour.</p>
 
 <div id="content" class="posts">
-	<?php foreach ($page->ressources()->toStructure() as $ressource): ?>
+	<?php foreach ($page->ressources()->toStructure()->sortBy('date', 'desc') as $ressource): ?>
 	  	<article>
 			<?php snippet('ressource', ['ressource' => $ressource]) ?>
 		</article>
