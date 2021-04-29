9efbe70bc4cf75d71c7b8637f579e463a6870d95
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-04-29 08:50:00 +0200

    Display citation in ressources

--- site/snippets/ressource.php
+++ site/snippets/ressource.php
@@ -1,8 +1,14 @@
+<?php if ($ressource->type() == "Citation") { ?>
+	<blockquote>
+		<p><?= $ressource->title()->kt() ?></p>
+		<p><small>— Par <span><?= $ressource->source() ?></small></p>
+	</blockquote>
+<?php } else { ?>
 <h3><a href="<?= $ressource->url() ?>" class="button"><?= $ressource->title() ?></a></h3>
 <?= $ressource->description()->kt() ?>
 
 <?php if ($ressource->source()->isNotEmpty()) : ?>
-<p>Par <span><?= $ressource->source() ?></span></p>
+<p><small>Par <span><?= $ressource->source() ?></span></small></p>
 <?php endif ?>
 
 <span><?= $ressource->type() ?></span>
@@ -19,4 +25,5 @@
 	<?php foreach ($ressource->thematique()->split() as $thematique): ?>
   		<li><a href="<?= url($page->url(), ['params' => ['thematique' => $thematique]]) ?>"><?= $thematique ?></a></li>
   	<?php endforeach ?>
-</ul>
+</ul>	
+<?php } ?>
