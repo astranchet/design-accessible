120f4162de8cc8acf2132aea6ef180036510f0c1
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-05-01 09:01:51 +0200

    Markup

--- site/templates/ressources.php
+++ site/templates/ressources.php
@@ -1,17 +1,27 @@
 <?php snippet('header') ?>
 
-<h2><?= $page->title() ?></h2>
+<div id="content">
 
-<p><b><?= $ressources->count() ?> ressources</b> sélectionnées avec amour.</p>
+  <div class="last__article">
+    <div class="container">
+      <h2 class="last__article-title">
+        ✨ <b><?= $ressources->count() ?> ressources</b> sélectionnées avec amour.
+      </h2>
 
-<?php snippet('filters') ?>
+      <?php snippet('filters') ?>
 
-<div id="content" class="posts">
-	<?php foreach ($ressources as $ressource): ?>
-	  	<article>
-			<?php snippet('ressource', ['ressource' => $ressource]) ?>
-		</article>
-	<?php endforeach ?>
+      <ul class="last__article-list">
+        <?php foreach ($ressources as $ressource): ?>
+          <li class="last__article-item">
+            <article class="card card--horizontal">
+              <?php snippet('ressource', ['ressource' => $ressource]) ?>
+            </article>
+          </li>
+        <?php endforeach ?>
+      </ul>
+
+    </div>
+  </div>
+
 </div>
-
 <?php snippet('footer') ?>
