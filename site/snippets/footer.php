d279a10f55d47cfecee7474a237d7cba472165b7
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-05-03 14:41:33 +0200

    Read menus from config

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -4,10 +4,13 @@
   <div class="container">
     <nav role="navigation" aria-label="Navigation de pied de page">
       <ul>
-        <li><a href="<?= page('accessibility-statement')->url() ?>">Accessibilité</a></li>
-        <li><a href="<?= page('about')->url() ?>">À propos</a></li>
-        <li><a href="<?= page('contribute')->url() ?>">Contribuer</a></li>
+        <?php foreach ($site->footerNav()->toPages() as $item) { ?>
+          <li><a href="<?= $item->url() ?>" <?php e($item->isActive(), 'aria-current="page"') ?>>
+            <?= $item->menu()->or($item->title()) ?>
+          </a></li>
+        <?php } ?>
       </ul>
+
       <ul>
         <li><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /> <small>Licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Partage dans les Mêmes Conditions 4.0 International</small></a>.</li>
         <li>Conçu avec amour 💜</li>
