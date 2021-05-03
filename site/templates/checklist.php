baa892a9036dfdc40388677735b0107b7f5cb8d0
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-05-03 23:08:11 +0200

    Add checklist panel

---
+++ site/templates/checklist.php
@@ -0,0 +1,49 @@
+<?php snippet('header') ?>
+
+<div id="content">
+    <div class="container">
+		<?= $page->text()->kt() ?>
+
+		<?php foreach ($page->categories()->toStructure() as $category): ?>
+			<h3><?= $category->title() ?></h3>
+
+			<ul>
+			<?php foreach ($page->checklist()->toStructure()->filterBy('category', $category->title()) as $item): ?>
+				<li>
+					<h4><?= $item->title() ?></h4>
+					<p><?= $item->desc()->kirbytext() ?></p>
+				</li>
+			<?php endforeach ?>
+			</ul>
+		<?php endforeach ?>
+
+
+
+
+		<h2 class="last__article-title">
+	        ✨ Pour aller plus loin
+	    </h2>
+
+		<ul class="last__article-list">
+		<?php foreach ($page->ressources()->toStructure() as $ressource): ?>
+			<li class="last__article-item">
+				<article class="card card--horizontal">
+					<h3 class="card__title">
+  						<a href="<?= $ressource->url() ?>"><?= $ressource->title() ?></a>
+					</h3>
+
+					<div class="card__description">
+						<?= $ressource->description()->kt() ?>
+					</div>
+
+					<p class="card__author">
+						Par <span><?= $ressource->source() ?></span>
+					</p>
+				</article>
+			</li>
+		<?php endforeach ?>
+		</ul>
+  	</div>	
+</div>
+
+<?php snippet('footer') ?>
