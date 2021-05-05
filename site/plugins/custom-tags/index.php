77e99995969ff7cd9bc434aba9d921a707775d6e
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-05-05 10:05:38 +0200

    Display hreflang

--- site/plugins/custom-tags/index.php
+++ site/plugins/custom-tags/index.php
@@ -3,5 +3,6 @@
 Kirby::plugin('your/plugin', [
     'tags' => [
         'card' => require_once __DIR__ . '/tags/card.php',
+        'link' => require_once __DIR__ . '/tags/link.php',
     ]
 ]);
---
+++ site/plugins/custom-tags/tags/link.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$originalTag = Kirby\Text\KirbyTag::$types['link'];
+return [
+    'attr' => array_merge($originalTag['attr'], ['hreflang']),
+    'html' => function($tag) {
+        if (empty($tag->lang) === false) {
+            $tag->value = Url::to($tag->value, $tag->lang);
+        }
+
+        return Html::a($tag->value, $tag->text, [
+        	'hreflang'	=> $tag->hreflang,
+            'rel'    	=> $tag->rel,
+            'class' 	=> $tag->class,
+            'role'   	=> $tag->role,
+            'title'  	=> $tag->title,
+            'target' 	=> $tag->target,
+        ]);
+    }
+];
