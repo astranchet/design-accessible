f3c7cb24eea848dc22db50765ef692af62539be7
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-05-17 11:14:12 +0200

    Fix ressources with : in title

--- bin/fetch_airtable.py
+++ bin/fetch_airtable.py
@@ -14,7 +14,6 @@ def get_env():
    # Récupérer les variables d'environnement (API Airtable)
 	env = join(dirname(__file__), '.env')
 	load_dotenv(env)
-	# print(os.getenv('AIRTABLE_BASE_ID'))
 
 def get_ressources():
 	api = airtable.Airtable(os.getenv('AIRTABLE_BASE_ID'), os.getenv('AIRTABLE_API_KEY'))
@@ -51,7 +50,7 @@ def print_ressources(ressources):
 			print("	source: " + ressource['fields']['Auteur'])
 
 		if 'Description' in ressource['fields']:
-			print("	description: " + ressource['fields']['Description'])
+			print("	description: "{}"".format(ressource['fields']['Description']))
 
 		if 'Langue' in ressource['fields']:
 			if ressource['fields']['Langue'] == 'Français':
@@ -71,32 +70,7 @@ def print_ressources(ressources):
 		if 'Date' in ressource['fields']:
 			print("	date: " + ressource['fields']['Date'])
 
-		# TODO : pthématique (Thématique)
-		# print(ressource)
-		# return
-
 
 get_env()
 ressources = get_ressources()
 print_ressources(ressources)
-
-# print(startups_airtable['records'][0]['fields']['ID'])
-
-
-# OrderedDict([('id', 'rec1ZjJ28Hy7zTpvy'), ('fields', OrderedDict([(
-# 'Nom', ' Écriture inclusive et accessibilité numérique'), 
-# ('URL', 'https://www.lalutineduweb.fr/ecriture-inclusive-accessibilite-numerique-jetdv2020/'), 
-# ('Langue', 'Français'), 
-# ('Type de ressource', ['Article']), 
-# ('Date', '2021-01-14'), ('Scope', 'Inclusivité'), ('Thématique', ['Écriture inclusive', 'Contenu']), ('Auteur', 'Julie Moynat')])), ('createdTime', '2021-04-12T01:17:19.000Z')])
-
-# - 
-#   title: What is your favorite one sentence definition of digital accessibility? 
-#   url: >
-#     https://twitter.com/LFLegal/status/1348777835670896640
-#   description: "Diverses définitions de l'accessibilité."
-#   source: LFLegal
-#   lang: Anglais 
-#   type: Thread Twitter
-#   phase: Idéation, Recherche
-#   thematique: Sensibilisation
