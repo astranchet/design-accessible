3b5babbb1848ffd19e1c7c2e0da09759a48dd725
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-05-17 22:45:49 +0200

    Split text into sections

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,9 +1,19 @@
 <?php snippet('header') ?>
 
 <div id="content" class="<?= $page->slug(); ?>">
-    <div class="container">
-		<?= $page->text()->kt() ?>
-	</div>
+	<?php foreach ($page->sections()->toStructure() as $section): ?>
+     	<div class="section <?= $section->style()->or("") ?>">
+	    	<div class="container">
+            	<?= $section->text()->kt() ?>
+          	</div>
+        </div>
+    <?php endforeach ?>
+
+	<?php if ($page->text()->isNotEmpty()) { ?>
+    	<div class="container">
+			<?= $page->text()->kt() ?>
+		</div>
+	<?php } ?>
 </div>
 
 <?php snippet('footer') ?>
