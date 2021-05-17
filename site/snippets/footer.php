69827e3565e96cb4eb599c16f2de7c3755327bbf
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-05-17 16:46:15 +0200

    Footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -3,7 +3,7 @@
 <footer role="contentinfo" id="footer">
   <div class="container">
     <nav role="navigation" aria-label="Navigation de pied de page">
-      <ul>
+      <ul class="footer__links">
         <?php foreach ($site->footerNav()->toPages() as $item) { ?>
           <li><a href="<?= $item->url() ?>" <?php e($item->isActive(), 'aria-current="page"') ?>>
             <?= $item->menu()->or($item->title()) ?>
@@ -11,9 +11,9 @@
         <?php } ?>
       </ul>
 
-      <ul>
-        <li><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /> <small>Licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Partage dans les Mêmes Conditions 4.0 International</small></a>.</li>
-        <li>Conçu avec amour 💜</li>
+      <ul class="footer__legals">
+        <li><p>Conçu avec amour 💜</p></li>
+        <li><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /> <small><!-- Licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Partage dans les Mêmes Conditions 4.0 International --></small></a></li>
       </ul>
     </nav>
   </div>
