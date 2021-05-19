4f1e019cf06ecd84dd279bb6a142ea8be188ba3d
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-05-19 11:00:02 +0200

    Display filters

--- site/snippets/filters.php
+++ site/snippets/filters.php
@@ -1,25 +1,23 @@
-<nav>
-  <ul>
+<nav class="filters" role="navigation" aria-label="Navigation par filtres">
+  <ul class="tags">
     <?php foreach($phases as $phase): ?>
-    <li>
+    <li class="tags__item tags__item-phase">
       <a href="<?= url($page->url(), ['params' => ['phase' => $phase]]) ?>">
         <?= html($phase) ?>
       </a>
     </li>
     <?php endforeach ?>
-  </ul>
-  <ul>
+
     <?php foreach($langs as $id => $lang): ?>
-    <li>
+    <li class="tags__item tags__item-lang">
       <a href="<?= url($page->url(), ['params' => ['lang' => $id]]) ?>">
         <?= html($lang) ?>
       </a>
     </li>
     <?php endforeach ?>
-  </ul>
-  <ul>
+
     <?php foreach($thematiques as $thematique): ?>
-    <li>
+    <li class="tags__item tags__item-thematique">
       <a href="<?= url($page->url(), ['params' => ['thematique' => $thematique['name']]]) ?>">
         <?= html($thematique['name']) ?> (<?= $thematique['count'] ?>)
       </a>
