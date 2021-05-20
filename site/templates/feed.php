6857b370963e16ab11ddcabe4aba56a39a40728e
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-05-20 22:58:01 +0200

    Fix header feed

--- site/templates/feed.php
+++ site/templates/feed.php
@@ -1,10 +1,13 @@
+<?php
+$kirby->response()->type('application/xml');
+?>
 <?xml version="1.0" encoding="utf-8" standalone="yes" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
     <title><?= $site->title(); ?></title>
     <link><?= $site->url(); ?></link>
     <description><?= $site->baseline(); ?></description>
-    <atom:link href="<?= $site->page('feed')->url() ?>" rel="self" type="application/rss+xml" />
+    <atom:link href="<?= $site->page('rss')->url() ?>" rel="self" type="application/rss+xml" />
 
     <?php foreach ($ressources->limit(15) as $ressource): ?>
     <item>
