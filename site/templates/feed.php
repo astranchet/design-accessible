5ff0476e2e76c259b42880a5ef8a3befe51889ac
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-07-05 15:32:28 +0200

    Add headers to rss feed

--- site/templates/feed.php
+++ site/templates/feed.php
@@ -3,6 +3,7 @@ $kirby->response()->type('application/xml');
 echo '<?xml version="1.0" encoding="utf-8" standalone="yes" ?>';
 echo '<?xml-stylesheet href="/assets/rss.xsl" type="text/xsl"?>';
 ?>
+<?php snippet('php-headers') ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
     <title><?= $site->title(); ?></title>
